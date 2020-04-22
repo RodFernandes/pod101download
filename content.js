e823a26bbed38e1054009d73e5f34d2add252149
RodFernandes <rodfernandes82@gmail.com>
2020-04-23 00:19:03 +0100

    second

--- content.js
+++ content.js
@@ -1,11 +1,29 @@
 console.log("Chrome Extension go");
 
-chrome.runtime.onMessage.addLIstener(gotMessage);
+chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse) {
   console.log(message.txt);
+
   let paragraphs = document.getElementsByTagName("p");
+
+  const color =
+    "#" +
+    getColor() +
+    getColor() +
+    getColor() +
+    getColor() +
+    getColor() +
+    getColor();
+
   for (elt of paragraphs) {
-    elt.style["background-color"] = "#FF00FF";
+    elt.style["background-color"] = color;
   }
+
+  console.log(color);
+}
+
+function getColor() {
+  let rnd = Math.floor(Math.random() * 16);
+  return rnd.toString(16);
 }
