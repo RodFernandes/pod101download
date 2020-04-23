7fa8ad9705fa7abb2280c090f58ba5b1bfb9165c
RodFernandes <rodfernandes82@gmail.com>
2020-04-23 22:48:48 +0100

    third

--- background.js
+++ background.js
@@ -1,10 +1,1 @@
 console.log("background running");
-chrome.browserAction.onClicked.addListener(buttonCliked);
-
-function buttonCliked(tab) {
-  let msg = {
-    txt: "hello",
-  };
-  chrome.tabs.sendMessage(tab.id, msg);
-  console.log(tab);
-}
--- content.js
+++ content.js
@@ -1,29 +1,48 @@
 console.log("Chrome Extension go");
 
-chrome.runtime.onMessage.addListener(gotMessage);
+getDialogues();
+getVacabulary();
+setDialoguesTextAll();
 
-function gotMessage(message, sender, sendResponse) {
-  console.log(message.txt);
+function sendMEssage(message) {}
 
-  let paragraphs = document.getElementsByTagName("p");
+function getDialogues() {
+  console.log("getDialogues");
+  const dialogues = document.getElementsByClassName("js-lsn3-play-dialogue");
 
-  const color =
-    "#" +
-    getColor() +
-    getColor() +
-    getColor() +
-    getColor() +
-    getColor() +
-    getColor();
-
-  for (elt of paragraphs) {
-    elt.style["background-color"] = color;
+  if (dialogues) {
+    let dataset = [];
+    for (dialogue of dialogues) {
+      dataset.push(dialogue.dataset.src);
+    }
+    console.log(dataset);
   }
-
-  console.log(color);
 }
 
-function getColor() {
-  let rnd = Math.floor(Math.random() * 16);
-  return rnd.toString(16);
+function getVacabulary() {
+  console.log("getVacabulary");
+  const vocabularies = document.getElementsByClassName(
+    "js-lsn3-play-vocabulary"
+  );
+
+  if (vocabularies) {
+    let dataset = [];
+    for (vocabulary of vocabularies) {
+      dataset.push(vocabulary.dataset.src);
+    }
+    console.log(dataset);
+  }
+}
+
+function setDialoguesTextAll() {
+  console.log("setDialoguesTextAll");
+  const all = document.getElementById("dialogue_tab_all_0");
+
+  if (all) {
+    console.log(all.childNodes[1]);
+    all.childNodes[1].click();
+    //all.setAttribute("aria-selected", true);
+    //all.setAttribute("class", "r101-navigation__current");
+    console.log("aria-selected, true");
+  }
 }
