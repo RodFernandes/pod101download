11e7a83ac19cd726c145e464e865576727b6de0c
RodFernandes <rodfernandes82@gmail.com>
2020-04-24 21:41:33 +0100

    add show Examples

--- content.js
+++ content.js
@@ -3,9 +3,10 @@ console.log("Chrome Extension go");
 start();
 
 function start() {
+  showDialoguesTextAll();
+  showExamples();
   getAudios("js-lsn3-play-dialogue");
   getAudios("js-lsn3-play-vocabulary");
-  setDialoguesTextAll();
 }
 
 function sendMEssage(message) {}
@@ -13,45 +14,20 @@ function sendMEssage(message) {}
 function getAudios(element) {
   console.log("getDialogues");
   const dialogues = document.getElementsByClassName(element);
+  //const domain = getDomainActive();
 
   if (dialogues) {
     let dataset = [];
     for (dialogue of dialogues) {
-      dataset.push(dialogue.dataset.src);
+      if (!dataset.includes(dialogue.dataset.src)) {
+        dataset.push(dialogue.dataset.src);
+      }
     }
     console.log(dataset);
   }
 }
 
-// function getDialogues() {
-//   console.log("getDialogues");
-//   const dialogues = document.getElementsByClassName("js-lsn3-play-dialogue");
-
-//   if (dialogues) {
-//     let dataset = [];
-//     for (dialogue of dialogues) {
-//       dataset.push(dialogue.dataset.src);
-//     }
-//     console.log(dataset);
-//   }
-// }
-
-// function getVacabulary() {
-//   console.log("getVacabulary");
-//   const vocabularies = document.getElementsByClassName(
-//     "js-lsn3-play-vocabulary"
-//   );
-
-//   if (vocabularies) {
-//     let dataset = [];
-//     for (vocabulary of vocabularies) {
-//       dataset.push(vocabulary.dataset.src);
-//     }
-//     console.log(dataset);
-//   }
-// }
-
-function setDialoguesTextAll() {
+function showDialoguesTextAll() {
   console.log("setDialoguesTextAll");
   const all = document.getElementById("dialogue_tab_all_0");
 
@@ -60,4 +36,34 @@ function setDialoguesTextAll() {
     all.childNodes[1].click();
     console.log("aria-selected, true");
   }
+}
+
+function showExamples() {
+  const examples = document.getElementsByClassName(
+    "lsn3-lesson-vocabulary__examples"
+  );
+  if (examples) {
+    //examples[0].click();
+    for (item of examples) {
+      if (item.type == "button") {
+        item.click();
+        //console.log(item);
+      }
+    }
+  }
+}
+
+function setDomain(url, domainActive) {
+  if (url.startsWith("/")) {
+    return domainActive + url;
+  }
+  return url;
+}
+
+function getDomainActive() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var tab = tabs[0];
+    var url = new URL(tab.url);
+    var domain = url.hostname;
+  });
 }
